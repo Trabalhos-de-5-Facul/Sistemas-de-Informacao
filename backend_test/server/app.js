c025370c2b8832b9dc4504ec49c9970975430d9e
GuGaCacau <guga_cacau@hotmail.com>
2022-04-23 15:40:10 -0300

    Implementação da REST API

---
+++ backend_test/server/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const rotaOngs = require('./routes/ongs');
+
+// Uso do Morgan para monitoramento de requisições
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false })); // Para dados simples
+app.use(bodyParser.json()); // Para dados JSON
+
+// Permissão de origem e cabeçalho para todos os servidores
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Header',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+    );
+
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+})
+
+// Chamada da rota das ONGs na aplicação
+app.use('/ongs', rotaOngs);
+
+// Tratamento de erro ao não encontrar uma Rota válida
+app.use((req, res, next) => {
+    const erro = new Error('Rota não encontrada.');
+    erro.status = 404;
+    next(erro);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
+module.exports = app;
