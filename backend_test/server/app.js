3b2e2c8127919a620d9bb40f768228071d78f29e
GuGaCacau <guga_cacau@hotmail.com>
2022-04-27 16:10:39 -0300

    Rota para Funcionários

--- backend_test/server/app.js
+++ backend_test/server/app.js
@@ -1,46 +1,48 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const rotaOngs = require('./routes/ongs');
+const rotaOngs = require("./routes/ongs");
+const rotaFuncionarios = require("./routes/funcionarios");
 
 // Uso do Morgan para monitoramento de requisições
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false })); // Para dados simples
 app.use(bodyParser.json()); // Para dados JSON
 
 // Permissão de origem e cabeçalho para todos os servidores
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Header',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    );
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
 
-    if(req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        return res.status(200).send({});
-    }
-    next();
-})
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).send({});
+  }
+  next();
+});
 
-// Chamada da rota das ONGs na aplicação
-app.use('/ongs', rotaOngs);
+// Chamada das rotas das ONGs e Funcionários na aplicação
+app.use("/ongs", rotaOngs);
+app.use("/funcionarios", rotaFuncionarios);
 
 // Tratamento de erro ao não encontrar uma Rota válida
 app.use((req, res, next) => {
-    const erro = new Error('Rota não encontrada.');
-    erro.status = 404;
-    next(erro);
+  const erro = new Error("Rota não encontrada.");
+  erro.status = 404;
+  next(erro);
 });
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    return res.send({
-        erro: {
-            mensagem: error.message
-        }
-    });
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      mensagem: error.message,
+    },
+  });
 });
 
 module.exports = app;
